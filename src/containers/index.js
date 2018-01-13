a938e6ea279da507e3fe957564d60dc81f6bdbea
Stanimir Dimitrov <stanchino@gmail.com>
2018-01-13 19:19:08 +0200

    Fixing the build scripts, code coverage and removed the ResetPasswordButton component

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,9 +1,8 @@
 import ConfirmationForm from "./ConfirmationForm";
 import Protected from "./Protected";
-import ResetPasswordButton from "./ResetPasswordButton";
 import SignInForm from "./SignInForm";
 import SignOutLink from "./SignOutLink";
 import SignUpForm from "./SignUpForm";
 import UnauthenticatedLink from "./UnauthenticatedLink";
 
-export { ConfirmationForm, Protected, ResetPasswordButton, SignInForm, SignOutLink, SignUpForm, UnauthenticatedLink }
+export { ConfirmationForm, Protected, SignInForm, SignOutLink, SignUpForm, UnauthenticatedLink }
